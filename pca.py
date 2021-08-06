ee769bfa9692f6368ec5fa5aaa1f3e72f3910a60
Clement Carre <clement.carre@bionomeex.com>
2021-08-06 11:44:17 +0200

    Add color argument

--- pca.py
+++ pca.py
@@ -1,16 +1,18 @@
 import tensorflow as tf
 import argparse
 import os
-
+import numpy as np
 
 parser = argparse.ArgumentParser(
     usage="%(prog)s [OPTION] -I Input file",
-    description="Sobel Edge Detection",
+    description="Acp Transformation of each pixels",
 )
 parser.add_argument(
     "-I", help="Input image", type=str
 )
-
+parser.add_argument(
+    "-c", help="Color order", type=str
+)
 parser.add_argument(
     "-o", help="Output filename", type=str, default=None
 )
@@ -33,7 +35,13 @@ _, eigen_vectors = tf.linalg.eigh(tf.matmul(img, img, transpose_a=True))
 
 img = tf.matmul(eigen_vectors, img, transpose_a=True, transpose_b=True)
 
-img = tf.reshape(img, (c, x, y))
+img2 = np.zeros((c, x * y), dtype=np.float32)
+
+img2[0, :] = img[int(args.c[0]), :]
+img2[1, :] = img[int(args.c[1]), :]
+img2[2, :] = img[int(args.c[2]), :]
+
+img = tf.reshape(img2, (c, x, y))
 
 img = tf.transpose(img, perm=(1, 2, 0))
 
